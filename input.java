c25ffdf2a78a7c87643673be88c108f7b1f9af77
Consx01 <soulcons30@gmail.com>
2024-03-02 14:32:49 +0530

    Initial program

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class input
+{
+    public static void main(String[]args)
+    {
+        Scanner gt = new Scanner(System.in);
+        int a;
+        
+        System.out.print("Enter NO. : ");
+        a = gt.nextInt();
+
+        System.out.println("Number is : "+a);
+
+    }
+}
